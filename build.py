86863940271dab554919e1741f796f783c104127
mxpro1996 <73088482+mxpro1996@users.noreply.github.com>
2024-11-04 22:05:24 +0800

    Update build.py

--- build.py
+++ build.py
@@ -151,8 +151,8 @@ def auto_update_idf(platform_name, module_name):
         if ret:
             raise Exception("git submodule update failed")
         print('Update completed')
-        ret = subprocess.call('sed -i '462s/0/1000/' ./esp-idf/components/lwip/lwip/src/api/tcpip.c, shell = True)
-        ret = subprocess.call('sed -n '462p' ./esp-idf/components/lwip/lwip/src/api/tcpip.c, shell = True)
+        ret = subprocess.call("sed -i '462s/0/1000/' ./esp-idf/components/lwip/lwip/src/api/tcpip.c", shell = True)
+        ret = subprocess.call("sed -n '462p' ./esp-idf/components/lwip/lwip/src/api/tcpip.c", shell = True)
         if ret:
             raise Exception("Patch wait-sem err")
         print('Patch completed')
